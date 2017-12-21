46ae01ff831df16f1ae40c783fd5cdfb052eb070
SebastienLattaignant62 <steeve.caroll@gmail.com>
2017-12-21 16:27:09 +0100

    premier jet : html css clear, debut du js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-<td onclick="this.style.background='#eeeeee'">
+
+
+console.log("start");
+  var joueur=1;
+
+
+  var annonce="joueur";
+  alert("joueur1");
+
+function game(id){
+  if(joueur==1){
+    document.getElementById("case"+id).className="player1";
+    joueur=2;
+    console.log("joueur1");
+      alert(annonce+"2");
+}
+else if(joueur==2){
+  document.getElementById("case"+id).className="player2";
+  joueur=1;
+  console.log("joueur2");
+  alert(annonce+"1");
+}
+  document.getElementById("case"+id).onclick=function(){
+  return false;
+  };
+}
+// var m="joueur";
+// alert(m);
+
+
+console.log("end");
